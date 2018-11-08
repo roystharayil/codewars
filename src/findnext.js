4414655ba8e195952d3ac762d560347b76d63cbb
roystharayil <roystharayil@gmail.com>
2018-11-08 22:06:36 +0530

    findNextSquare

--- src/findnext.js
+++ src/findnext.js
@@ -1,10 +1,11 @@
 function findNextSquare(sq) {
-    var sqrt = Math.sqrt(sq);
-    if(sq%sqrt == 0) {
-        return Math.pow(sqrt++);
+    var sqroot = Math.sqrt(sq);
+    if(sq%sqroot == 0) {
+        return Math.pow(sqroot+1,2);
     }
     else{ 
     return -1; }
   }
 
-  console.log(findNextSquare(121));
+  //console.log(findNextSquare(120));
+console.log(Math.sqrt(120)%1 ? -1 : Math.pow(Math.sqrt(121)+1, 2));
