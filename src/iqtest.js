78cfc9b250465a366ad1d578f12a6c36d9987553
roystharayil <roystharayil@gmail.com>
2018-11-08 18:17:52 +0530

    passing index value

--- src/iqtest.js
+++ src/iqtest.js
@@ -5,19 +5,23 @@ function iqtest(numbers){
 
     num.forEach((no, index) => {
         if(no == 1){
-            odd.push(no); 
+            odd.push(index); 
         }
         if(no%2) {
-            odd.push(no);
+            odd.push(index);
         }         
         if(no%2 == 0) {
-            even.push(no);
+            even.push(index);
         }
     });
-
-    return odd+" "+even;
-    
-
+    if(odd.length == 1){
+        var p = Number(odd[0]);
+        return p+1;
+    }
+    if(odd.length > 1) {
+        var q = Number(even[0]);
+        return q+1;
+    }
 }
 
-console.log(iqtest("2 4 7 8 10"));
+console.log(iqtest("1 2 2"));
