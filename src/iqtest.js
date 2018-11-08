2ce469b00f1224dd1566af13bc410c4e5ff795f0
roystharayil <roystharayil@gmail.com>
2018-11-08 18:10:38 +0530

    pushing number

--- src/iqtest.js
+++ src/iqtest.js
@@ -1,4 +1,22 @@
 function iqtest(numbers){
+    var num = numbers.split(" ");
+    var odd = [];
+    var even = [];
+
+    num.forEach((no, index) => {
+        if(no == 1){
+            odd.push(no); 
+        }
+        if(no%2) {
+            odd.push(no);
+        }         
+        if(no%2 == 0) {
+            even.push(no);
+        }
+    });
+
+    return odd+" "+even;
+    
 
 }
 
