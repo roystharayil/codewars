deb1c1ce6f3e091dbb9e6c435de421d8040dcb23
roystharayil <roystharayil@gmail.com>
2018-11-08 22:33:22 +0530

    array fx

--- src/uniqueOrder.js
+++ src/uniqueOrder.js
@@ -1,9 +1,19 @@
 var uniqueInOrder=function(iterable){
-    var arr = iterable.split(" ");
-    console.log(arr);
+    if(typeof(iterable) == 'string') {    
+    var arr = iterable.split("");}
+
+    if(typeof(iterable) == 'object') {
+    var arr = iterable;}
+
+    var result = [];
     
-    
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] != arr[i+1]) {
+            result.push(arr[i]);
+        }        
+    }
+    return result;
   }
-  uniqueInOrder('AAAABBBCCDAABBB');
-  //console.log(uniqueInOrder('AAAABBBCCDAABBB'));
+  console.log(uniqueInOrder([1,2,2,3,3]));
+  
   
