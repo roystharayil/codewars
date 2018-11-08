9ee26dcf8cea9caf7f5e1e32d7426e8b2b3a1d11
roystharayil <roystharayil@gmail.com>
2018-11-08 22:46:30 +0530

    refracted

--- src/uniqueOrder.js
+++ src/uniqueOrder.js
@@ -1,15 +1,16 @@
-var uniqueInOrder=function(iterable){
-    if(typeof(iterable) == 'string') {    
-    var arr = iterable.split("");}
+var uniqueInOrder=function(arr){
+    // if(typeof(iterable) == 'string') {    
+    // var arr = iterable.split("");}
 
-    if(typeof(iterable) == 'object') {
-    var arr = iterable;}
+    // if(typeof(iterable) == 'object') {
+    // var arr = iterable;}
 
     var result = [];
-    
+    var last
+   
     for (let i = 0; i < arr.length; i++) {
-        if(arr[i] != arr[i+1]) {
-            result.push(arr[i]);
+        if(arr[i] !== last) {
+            result.push(last = arr[i]);
         }        
     }
     return result;
