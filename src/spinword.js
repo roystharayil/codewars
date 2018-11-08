3551a2036609c63bccdbae3923fa2c84f56ac3dc
roystharayil <roystharayil@gmail.com>
2018-11-08 23:36:33 +0530

    spinword completed

--- src/spinword.js
+++ src/spinword.js
@@ -1,6 +1,20 @@
-function spinWords(){
+function spinWords(words){
+    var wArr = words.split(" ");
+    var arr1;
+    var arr2 = [];
+    wArr.forEach(element => {
+        if(element.length >= 5) {
+            var splitString = element.split("");
+            var arr1 = splitString.reverse();
+            var joinArray = arr1.join("");
+            arr2.push(joinArray); 
+        }
+        else {
+            arr2.push(element);
+        }
+    }); 
+    return arr2.join(" ");
+}
 
-  }
-
-  console.log(spinWords("Hey fellow warriors"));
+console.log(spinWords("Hey fellow warriors"));
   
